6e3fc8e9b203b07dc6cef2e49267702960eab452
RisamAlbanjumasy <risam1984@gmail.com>
2026-05-30 07:54:06 +0700

    rebuild

--- models/category.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Category struct {
-	ID          string `json:"uuid"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Published   bool   `json:"published"`
-}
--- models/tag.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-
-type Tag struct {
-	ID        int       `json:"id"`
-	Tag       string    `json:"tag"`
-	CreatedAt time.Time `json:"created_at"`
-}
